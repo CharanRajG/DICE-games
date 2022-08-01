33a7b26a68f8db8cf6cd7f6532344ac7581848cf
Nuthan Prasad <nuthan1159@gmail.com>
2022-08-01 15:33:11 +0530

    Add files via upload

---
+++ t-index.js
@@ -0,0 +1,26 @@
+let k1 = Math.floor(Math.random()*6) + 1;
+
+let randomdice1 ="dice" + k1 + ".png";
+
+let k2 = Math.floor(Math.random()*6) + 1;
+
+let randomdice2 = "dice" + k2 + ".png";
+
+document.querySelectorAll("img")[0].src = randomdice1 ;
+
+document.querySelectorAll("img")[1].src = randomdice2 ;
+
+if (k1 > k2){
+    document.getElementById("h1").innerHTML = "Player 1 won 🚩";
+    document.getElementById("p1").classList.add("p1x");
+}
+
+else if (k1 < k2){
+    document.getElementById("h1").innerHTML = "Player 2 won 🚩";
+    document.getElementById("p2").classList.add ("p2x");
+}
+else{
+    document.getElementById("h1").innerHTML = "🚩 DRAW 🚩";
+    document.getElementById("p1").classList.add("p1x");
+    document.getElementById("p2").classList.add("p2x");
+}
